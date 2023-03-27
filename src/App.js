b723ebf6fd197044c57c9632a1e1877418754922
Lotte Koblens <lotte.koblens@hva.nl>
2023-03-27 21:40:04 +0200

    removed unused code

--- src/App.js
+++ src/App.js
@@ -16,21 +16,6 @@ const App = () => {
     SetData(showsJson);
   };
 
-
-  // const ComedyShows = async () => {
-  //   let array = [];
-
-  //   let data = await GetAllShowData()
-  //   data.forEach(element => {
-  //     if (element.genres.includes('Comedy')) {
-  //       array.push(element)
-  //     }
-  //   });
-
-  //   SetData(array)
-  // }
-  // ComedyShows(Data)
-
   // const HighestRating = async () => {
   //   let array = []
 
@@ -47,24 +32,9 @@ const App = () => {
   // let highest_rating = HighestRating()
   // console.log(highest_rating)
 
-  // const data = getData()
-  // const data2 = getShowData()
-  // data2.then(shows => {
-  //   shows.forEach(element => {
-  //     return element.name
-  //   });
-  // })
-  // console.log(data2)
-  // console.log(data)
-
 
   return (<div>
     <h1>React app</h1>
-    {/* <ul>
-      {Data.map((item) => (
-        <li key={item.id}>{item.name}</li>
-      ))}
-    </ul> */}
     <ComedyShows data={Data} />
   </div>
   )
